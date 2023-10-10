b040b6446575014ab350b5630c23dba38d4db9a6
Elkin Gabriel Niño Sanchez <latinohablantes@gmail.com>
2023-10-09 21:33:56 -0500

    ejercicio matrices y ejercicio test viernes

---
+++ Test/ordenando-diccionario.py
@@ -0,0 +1,21 @@
+productos = {
+    1: {
+        "nombre": "pantalon",
+        "precio": 125000,
+        "cantidad": 10
+    },
+    2: {
+        "nombre": "camisa",
+        "precio": 100000,
+        "cantidad": 30
+    },
+    3: {
+        "nombre": "pantaloneta",
+        "precio": 50000,
+        "cantidad": 5
+    }
+}
+
+print(productos)
+
+productos.items
