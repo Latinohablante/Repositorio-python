525e8e3aa33e282526de630e58b60b2cc508b043
Elkin Gabriel <latinohablantes@gmail.com>
2023-10-11 21:40:50 -0500

    datos empleado

--- Temas de Clase/Archivos/ejercicio empleados/leer-datos-empleados.py
+++ Temas de Clase/Archivos/ejercicio empleados/leer-datos-empleados.py
@@ -7,13 +7,13 @@ Listas = []
 for linea in fd:
     Listas.append(linea.split(","))
 
-# for i in range(1, len(Listas)):
-#     for k in range(len(Listas[0])):
-#         print(Listas[0][k].strip(), end=": ")
-#         print(Listas[i][k].strip())
-#     print("="*30)
+for i in range(1, len(Listas)):
+    for k in range(len(Listas[0])):
+        print(Listas[0][k].strip(), end=": ")
+        print(Listas[i][k].strip())# .strip() -> retira el \n
+    print("="*30)
 
 fd.close()
 
-print(Listas[0])
+#print(Listas[0])
 #print(Listas[0][2])
